3dfa74914769ee6c54f2d94470a5a99d9b26af27
Osama <107386980+osSidd@users.noreply.github.com>
2024-05-19 21:03:12 +0530

    added variables

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-      System.out.println("Hello World");
+      final String message = "Hello World";
+      int num = 23;
+      // float dec = 23.2;
+      float ds = 23.23f;
+      boolean My = true;
+      char ch = 'D';
+      int we;
+      we = 234;
+      System.out.println(we);
     }
   }
