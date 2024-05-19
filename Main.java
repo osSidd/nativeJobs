4f009f9a573a21825db62890b328df27e17a1d21
Osama <107386980+osSidd@users.noreply.github.com>
2024-05-19 21:18:58 +0530

    variables and errors

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
       final String message = "Hello World";
       int num = 23;
-      // float dec = 23.2;
+      // float dec = 23.2; Error
       float ds = 23.23f;
       boolean My = true;
       char ch = 'D';
