b87bbd3ead71c4a6b3f35aac43628cb1f463f622
Osama <107386980+osSidd@users.noreply.github.com>
2024-05-20 21:13:06 +0530

    data type

--- Main.java
+++ Main.java
@@ -1,13 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-      final String message = "Hello World";
-      int num = 23;
-      // float dec = 23.2; Error
-      float ds = 23.23f;
-      boolean My = true;
-      char ch = 'D';
-      int we;
-      we = 234;
-      System.out.println(we);
+      byte no = 123;
+      System.out.println(no);
     }
   }
