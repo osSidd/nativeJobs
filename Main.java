46a44b0f6d2cb30c1279c6d3e0838545c373a1e0
Osama <107386980+osSidd@users.noreply.github.com>
2024-05-21 21:51:34 +0530

    narrowing casting

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String[] args) {
       double num = 9.23;
+      int n = 23;
       byte no = (byte) num;
       System.out.println(no);
       System.out.println(9/(float)2);
+      double nwer = n;
 
     }
   }
