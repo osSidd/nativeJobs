fd399662b62258db1eb0643a78f25289cd70703f
Osama <107386980+osSidd@users.noreply.github.com>
2024-05-21 22:21:06 +0530

    string methods

--- Main.java
+++ Main.java
@@ -1,11 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-      double num = 9.23;
-      int n = 23;
-      byte no = (byte) num;
-      System.out.println(no);
-      System.out.println(9/(float)2);
-      double nwer = n;
+     
+      String str = "Hello World";
+      System.out.println(str.toUpperCase());
+      System.out.println(str.indexOf("Hello"));
+      System.out.println(str.concat(" How are \fyou?"));
+     
+      // double num = 9.23;
+      // int n = 23;
+      // byte no = (byte) num; //narrowing casting
+      // System.out.println(no);
+      // System.out.println(9/(float)2);
+      // double nwer = n; //widening casting
 
     }
   }
